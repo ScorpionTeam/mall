3f72c421eba66b29e9dc3b3fe1c54124dc625693
Jarven <769558579@qq.com>
2017-09-29 15:57:24 +0800

    封装对象

---
+++ src/main/java/com/scoprion/mall/good/service/GoodService.java
@@ -0,0 +1,30 @@
+package com.scoprion.mall.good.service;
+
+import com.scoprion.mall.domain.Good;
+import com.scoprion.result.PageResult;
+
+import java.util.List;
+
+/**
+ * Created on 2017/9/29.
+ */
+public interface GoodService {
+
+    /**
+     * 首页展示  限时购商品列表
+     *
+     * @return
+     */
+    List<Good> findLimit4ByTimeGoods();
+
+    /**
+     * 查询限时购买商品   分页展示
+     *
+     * @param pageNo   当前页
+     * @param pageSize 每页条数
+     * @return
+     */
+    PageResult findByPageAndLimit(int pageNo, int pageSize);
+
+
+}
