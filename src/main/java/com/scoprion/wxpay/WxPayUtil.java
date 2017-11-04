e33d525d48131c8ef83a89b43d51b29635ce0d25
Jarven <769558579@qq.com>
2017-11-04 21:19:27 +0800

    封装支付

--- src/main/java/com/scoprion/wxpay/WxPayUtil.java
+++ src/main/java/com/scoprion/wxpay/WxPayUtil.java
@@ -17,12 +17,37 @@ public class WxPayUtil {
         String requertUrl = WxPayConfig.WECHAT_ORDER_QUERY_URL;
         String method ="POST";
         String responseString = WxUtil.httpsRequest(requertUrl,method,requestXMLData);
-
+        UnifiedOrderResponseData responseData = WxPayUtil.castDataToXMLString()
         return null;
     }
 
     private static String castDataToXMLString(Object object) {
         XStream xStream = new XStream(new DomDriver("UTF-8", new XmlFriendlyNameCoder("-_", "_")));
         return xStream.toXML(object);
+    }
+
+    /**
+     * 把XML字符串转换为统一下单接口返回数据对象
+     * @param responseString
+     * @return
+     * return:UnifiedOrderResponseData
+     */
+    private static UnifiedOrderResponseData castXMLStringToUnifiedOrderResponseData(String responseString){
+        XStream xStream = new XStream(new DomDriver());
+        xStream.alias("xml", UnifiedOrderResponseData.class);
+        xStream.processAnnotations(UnifiedOrderResponseData.class);
+        return (UnifiedOrderResponseData) xStream.fromXML(responseString);
+    }
+
+    /**
+     * 把XML字符串转换为统一下单回调接口请求数据对象
+     * @param requestString
+     * @return
+     * return:UnifiedOrderNotifyRequestData
+     */
+    public static UnifiedOrderNotifyRequestData castXMLStringToUnifiedOrderNotifyRequestData(String requestString){
+        XStream xStream = new XStream(new DomDriver());
+        xStream.alias("xml", UnifiedOrderNotifyRequestData.class);
+        return (UnifiedOrderNotifyRequestData) xStream.fromXML(requestString);
     }
 }
