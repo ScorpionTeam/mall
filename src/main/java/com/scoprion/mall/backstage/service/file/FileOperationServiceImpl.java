a52e0b8d8fc574cc52fd30ef616cd7965ddc7350
ycj <532446306@qq.com>
2017-11-23 15:51:29 +0800

    解决图片不能删除的bug

--- src/main/java/com/scoprion/mall/backstage/service/file/FileOperationServiceImpl.java
+++ src/main/java/com/scoprion/mall/backstage/service/file/FileOperationServiceImpl.java
@@ -14,6 +14,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.imageio.ImageIO;
@@ -104,6 +105,7 @@ public class FileOperationServiceImpl implements FileOperationService {
      *
      * @param imageName 图片名称 /mall/brandimage/1510215075689.jpg
      */
+    @Transactional(rollbackFor = Exception.class)
     @Override
     public BaseResult deleteImage(String imageName) throws IOException {
         if (StringUtils.isEmpty(imageName)) {
