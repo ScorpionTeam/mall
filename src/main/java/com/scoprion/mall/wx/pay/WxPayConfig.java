1d06c0e146ce1bb48be34ff425765c6104a2a4fc
hmw <123456>
2017-11-30 11:42:53 +0800

    Merge branch 'master' of D:\JavaWorkspace\mall with conflicts.

--- src/main/java/com/scoprion/mall/wx/pay/WxPayConfig.java
+++ src/main/java/com/scoprion/mall/wx/pay/WxPayConfig.java
@@ -32,7 +32,7 @@ public class WxPayConfig {
      * 微信商户平台支付结果通知URL
      * 线上环境
      */
-    public final static String NOTIFY_URL = "http://w4g36q.natappfree.cc/mall/wx/jsapi/callback/pay";
+    public final static String NOTIFY_URL = "http://fkabkf.natappfree.cc/mall/wx/jsapi/callback/pay";
     /**
      * 统一下单URL
      */
@@ -62,6 +62,6 @@ public class WxPayConfig {
     /**
      * 访问趋势
      */
-    public final static String VISIT_REND = "https://api.weixin.qq.com/datacube/getweanalysisappiddailyvisittrend?access_token=" + APP_SECRET;
+    public final static String VISIT_REND="https://api.weixin.qq.com/datacube/getweanalysisappiddailyvisittrend?access_token="+APP_SECRET;
 
 }
