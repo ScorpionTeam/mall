aa9ec6f9a1f0bf22df8f97fab6b1384f415bb50c
hmy <1049313183@qq.com>
2017-12-05 13:01:42 +0800

    修改默认地址bug

--- src/main/java/com/scoprion/config/MallConfig.java
+++ src/main/java/com/scoprion/config/MallConfig.java
@@ -3,14 +3,24 @@ package com.scoprion.config;
 import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.alibaba.fastjson.support.config.FastJsonConfig;
 import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
+import com.rabbitmq.client.Channel;
 import com.scoprion.intercepter.MallInterceptor;
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.ChannelAwareMessageListener;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
 
 import java.util.List;
 
@@ -63,5 +73,80 @@ public class MallConfig extends WebMvcConfigurerAdapter {
                 .allowedOrigins("*")
                 .allowedHeaders("*")
                 .allowedMethods("*");
+    }
+
+    public static final String EXCHANGE ="spring-boot-exchange";
+    public static final String ROUTINGKEY="spring-boot-routingKey";
+
+    @Bean
+    public ConnectionFactory connectionFactory(){
+        CachingConnectionFactory connectionFactory=new CachingConnectionFactory();
+        connectionFactory.setAddresses("localhost");
+        connectionFactory.setUsername("guest");
+        connectionFactory.setPassword("guest");
+        connectionFactory.setVirtualHost("/");
+        connectionFactory.setPublisherConfirms(true);
+        return connectionFactory;
+    }
+
+    @Bean
+    @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+    /**
+     * 必须是prototype
+      */
+    public RabbitTemplate rabbitTemplate() {
+        RabbitTemplate template = new RabbitTemplate(connectionFactory());
+        return template;
+    }
+
+    /**
+     * 针对消费者配置
+     * 1. 设置交换机类型
+     * 2. 将队列绑定到交换机
+     *
+     *
+     FanoutExchange: 将消息分发到所有的绑定队列，无routingKey的概念
+     HeadersExchange ：通过添加属性key-value匹配
+     DirectExchange:按照routingKey分发到指定队列
+     TopicExchange:多关键字匹配
+     */
+    @Bean
+    public DirectExchange defaultExchange() {
+        return new DirectExchange(EXCHANGE);
+    }
+
+    /**
+     * 队列持久
+     * @return
+     */
+    @Bean
+    public Queue queue() {
+        return new Queue("spring-boot-queue", true);
+    }
+
+    @Bean
+    public Binding binding() {
+        return BindingBuilder.bind(queue()).to(defaultExchange()).with(MallConfig.ROUTINGKEY);
+    }
+
+    @Bean
+    public SimpleMessageListenerContainer messageContainer() {
+        SimpleMessageListenerContainer container = new SimpleMessageListenerContainer(connectionFactory());
+        container.setQueues(queue());
+        container.setExposeListenerChannel(true);
+        container.setMaxConcurrentConsumers(1);
+        container.setConcurrentConsumers(1);
+        //设置确认模式手工确认
+        container.setAcknowledgeMode(AcknowledgeMode.MANUAL);
+        container.setMessageListener(new ChannelAwareMessageListener () {
+            @Override
+            public void onMessage(Message message, Channel channel) throws Exception {
+                byte[] body = message.getBody();
+                System.out.println("receive msg : " + new String(body));
+                //确认消息成功消费
+                channel.basicAck(message.getMessageProperties().getDeliveryTag(), false);
+            }
+        });
+        return container;
     }
 }
