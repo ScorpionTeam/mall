bef666d0bf03087978170da51fb9896b01ef1712
Jarven <769558579@qq.com>
2017-12-06 17:35:11 +0800

    重写RequestMapping  导致bug修复

--- src/main/java/com/scoprion/config/RequestMappingHandlerSupport.java
+++ src/main/java/com/scoprion/config/RequestMappingHandlerSupport.java
@@ -3,8 +3,11 @@ package com.scoprion.config;
 import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.alibaba.fastjson.support.config.FastJsonConfig;
 import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
+import com.scoprion.intercepter.MallInterceptor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
@@ -16,6 +19,24 @@ import java.util.List;
  */
 @Configuration
 public class RequestMappingHandlerSupport extends WebMvcConfigurationSupport {
+
+    @Bean
+    public MallInterceptor getMallInterceptor() {
+        return new MallInterceptor();
+    }
+
+    /**
+     * 注入拦截器
+     *
+     * @param registry
+     */
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(getMallInterceptor());
+        super.addInterceptors(registry);
+    }
+
+
     @Override
     public RequestMappingHandlerMapping requestMappingHandlerMapping() {
         RequestMappingHandlerMapping handlerMapping = new MallRequestMappingHandler();
