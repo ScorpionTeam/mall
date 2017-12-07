bf1d4c350ace007760db732344ba051870644853
Jarven <769558579@qq.com>
2017-12-07 15:05:20 +0800

    seller

---
+++ src/main/java/com/scoprion/mall/seller/SellerController.java
@@ -0,0 +1,18 @@
+package com.scoprion.mall.seller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author by kunlun
+ * @created on 2017/12/7.
+ */
+
+@RequestMapping("/seller")
+@RestController
+public class SellerController {
+
+
+
+
+}
