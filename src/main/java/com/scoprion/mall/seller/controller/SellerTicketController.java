82eb324fa01d41e38fc575ddaf6e16d6669e1c88
FK <3198141550@qq.com>
2017-12-13 10:32:56 +0800

    商户优惠卷列表

--- src/main/java/com/scoprion/mall/seller/controller/SellerTicketController.java
+++ src/main/java/com/scoprion/mall/seller/controller/SellerTicketController.java
@@ -1,20 +1,19 @@
 package com.scoprion.mall.seller.controller;
 
-import com.scoprion.annotation.Access;
 import com.scoprion.mall.seller.service.ticket.SellerTicketService;
 import com.scoprion.result.PageResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author by fk
  * @created on 2017/12/13.
  */
-@RequestMapping("/seller/ticket")
 @RestController
+@RequestMapping("/seller/ticket")
 public class SellerTicketController {
 
     @Autowired
@@ -28,9 +27,10 @@ public class SellerTicketController {
      * @param sellerId
      * @return
      */
-    @Access
     @GetMapping("/findAll/{pageNo}/{pageSize}/{sellerId}")
-    public PageResult findAll(int pageNo, int pageSize, Long sellerId) {
+    public PageResult findAll(@PathVariable("pageNo") int pageNo,
+                              @PathVariable("pageSize") int pageSize,
+                              @PathVariable("sellerId") Long sellerId) {
         return sellerTicketService.findAll(pageNo, pageSize, sellerId);
     }
 }
